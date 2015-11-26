b22599f4b38c49d143bedc8d4fbd83f873dd6263
Tim Bennett <bennetimo@gmail.com>
2015-11-26 00:40:18 +0000

    add fragment with empty grid view

--- app/src/main/java/net/tbennett/popularmovies/MainActivity.java
+++ app/src/main/java/net/tbennett/popularmovies/MainActivity.java
@@ -5,9 +5,19 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private final String OVERVIEW_FRAGMENT_TAG = "FTAG_OVERVIEW";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            //Add the overview fragment to the view
+            getSupportFragmentManager()
+                .beginTransaction()
+                .add(R.id.container, new OverviewFragment(), OVERVIEW_FRAGMENT_TAG)
+                .commit();
+        }
     }
 }
---
+++ app/src/main/java/net/tbennett/popularmovies/OverviewFragment.java
@@ -0,0 +1,22 @@
+package net.tbennett.popularmovies;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class OverviewFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_overview, container, false);
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
